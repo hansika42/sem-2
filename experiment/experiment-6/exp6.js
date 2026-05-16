449145a66b65620beb3f49a9ec3c03e3d690c19e
hansika42 <shansika486@gmail.com>
2026-05-16 19:47:55 +0530

    experiment-6

--- experiment/experiment-6/exp6.js
+++ experiment/experiment-6/exp6.js
@@ -1,51 +1,68 @@
 let heading = document.getElementById("heading");
-let input = document.getElementById("inputText");
+let input = document.getElementById("userText");
 let para = document.getElementById("para");
 
-//change heading text 
-document.getElementById("changeTextBtn").onclick= 
+let fontsize = 32;
+
+// change heading text
+document.getElementById("changeTextBtn").onclick =
 function () {
     heading.innerText = input.value;
 };
 
-//input change event 
+// input change event
 input.onchange = function () {
     console.log("input changed:", input.value);
 };
 
-//change background color
-document.getElementById("bgColorBtn").
-addEventListener("click", function() {
+// change background color
+document.getElementById("bgColorBtn")
+.addEventListener("click", function () {
+
     document.body.style.backgroundColor = "lightblue";
     document.body.style.fontFamily = "Arial";
 });
 
-//change font size of heading
-document.getElementById("fontSizeBtn").
-addEventListener("click", function() {
-    fontsize +=2;
-    heading.style.fontsize = fontsize + "px";
-    heading.style.maxFontSize = "80px";
+// change font size
+document.getElementById("fontSizeBtn")
+.addEventListener("click", function () {
+
+    fontsize += 2;
+    heading.style.fontSize = fontsize + "px";
 });
 
-// show/ hide
-let isvisible = true;
+// show/hide paragraph
+let isVisible = true;
 
-document.getElementById("toggleParaBtn").onclick = function () {
+document.getElementById("toggleBtn").onclick =
+function () {
+
     if (isVisible) {
         para.style.display = "none";
         isVisible = false;
+    } else {
+        para.style.display = "block";
+        isVisible = true;
     }
-
 };
 
-// reset page 
-document.getElementById("resetBtn").onclick = 
+// reset page
+document.getElementById("resetBtn").onclick =
 function () {
-    onhover
+
+    heading.innerText = "Welcome to My Page";
+    document.body.style.backgroundColor = "#f4f4f4";
+    heading.style.fontSize = "32px";
+    heading.style.color = "black";
+    para.style.display = "block";
+
+    input.value = "";
+
+    fontsize = 32;
+    isVisible = true;
 };
 
-//mouse hover event
-heading.onmouseover = function (){
+// mouse hover event
+heading.onmouseover = function () {
     heading.style.color = "red";
 };
